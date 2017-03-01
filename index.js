0114a6e06715313739548d64917a560fa5c7d9cb
Jacob Buck <jacob_buck@bnz.co.nz>
2017-03-01 15:38:22 +1300

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var domParse = require('dom-parse');
+var domToVdom = require('./lib/dom-to-vdom');
+
+module.exports = function(html, options) {
+  options = options || {};
+  var parser = options.parser || domParse;
+  return domToVdom(parser(html))
+};
