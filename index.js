d90f5796be00ffc5254b13f41220cf31b1195af4
Jacob Buck <buck.jacob@gmail.com>
2017-04-28 00:11:38 +1200

    Add parserOptions option

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ var domToVdom = require('./lib/dom-to-vdom');
 module.exports = function(html, options) {
   options = options || {};
   var parser = options.parser || domParse;
-  return domToVdom(parser(html))
+  return domToVdom(parser(html, options.parserOptions))
 };
