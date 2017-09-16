3592b419c2d69ebde4f80b1fc6bcbf8939bafa5e
Jacob Buck <buck.jacob@gmail.com>
2017-09-16 12:30:41 +1200

    Update options passing

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import domParse from "dom-parse";
 import domToVdom from "./dom-to-vdom";
 
-const renderMarkup = (html, options = {}) => {
-  const parser = options.parser || domParse;
-  return domToVdom(parser(html, options.parserOptions));
-};
+const renderMarkup = (html, { parser = domParse, parserOptions, ...options }) =>
+  domToVdom(parser(html, parserOptions), options);
 
 export default renderMarkup;
