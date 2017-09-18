a24e267152a9702f8d700d87089677c80c38da7b
Jacob Buck <buck.jacob@gmail.com>
2017-09-18 12:17:56 +1200

    Remove parserOptions option

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import domParse from "dom-parse";
 import domToVdom from "./dom-to-vdom";
 
-const renderMarkup = (html, { parser = domParse, parserOptions, ...options }) =>
-  domToVdom(parser(html, parserOptions), options);
+const renderMarkup = (html, { parser = domParse, ...options }) =>
+  domToVdom(parser(html), options);
 
 export default renderMarkup;
