d13615682a353533b8eaacd0af5d8c13ba3478fd
Jacob Buck <buck.jacob@gmail.com>
2018-08-06 18:56:28 +1200

    Prettier

--- src/attrs-to-props.js
+++ src/attrs-to-props.js
@@ -1,5 +1,5 @@
-import cssToStyle from "css-to-style";
-import attrToPropName from "./attr-to-prop-name";
+import cssToStyle from 'css-to-style';
+import attrToPropName from './attr-to-prop-name';
 
 const attrsToProps = attrs =>
   [...attrs].reduce((props, attr) => {
@@ -7,21 +7,21 @@ const attrsToProps = attrs =>
     let value = attr.value;
 
     // Disallow event attributes
-    if (name.substr(0, 2) === "on") {
+    if (name.substr(0, 2) === 'on') {
       return props;
     }
 
     let propName;
-    if (["aria-", "data-"].includes(name.substr(0, 5))) {
+    if (['aria-', 'data-'].includes(name.substr(0, 5))) {
       propName = name;
-    } else if ("style" === name) {
-      propName = "style";
+    } else if ('style' === name) {
+      propName = 'style';
       value = cssToStyle(value);
     } else {
       propName = attrToPropName(name) || name;
     }
 
-    props[propName] = value === "" ? true : value;
+    props[propName] = value === '' ? true : value;
 
     return props;
   }, {});
