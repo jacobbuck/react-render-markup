b6dcc9aaa09b50fabc6a5ebad3e5e50ef5a4cb96
Jacob Buck <buck.jacob@gmail.com>
2018-09-27 21:39:17 +1200

    Prettier index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import parseDom from './parse-dom';
 import domToVdom from './dom-to-vdom';
 
-const renderMarkup = (html, options = {}) => 
-  html && typeof html === 'string'
-    ? domToVdom(parseDom(html), options)
-    : null;
+const renderMarkup = (html, options = {}) =>
+  html && typeof html === 'string' ? domToVdom(parseDom(html), options) : null;
 
 export default renderMarkup;
-
