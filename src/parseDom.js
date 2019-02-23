1599f1afa4d57d20850b52ea8465833e1f96ce0c
Jacob Buck <buck.jacob@gmail.com>
2019-02-23 23:36:37 +1300

    Remove jsdom due to inflating bundle size. Add docs on using server-side.

--- src/parseDom.js
+++ src/parseDom.js
@@ -1,13 +1,4 @@
-let DOMParser;
-if (typeof window !== 'undefined' && 'DOMParser' in window) {
-  DOMParser = window.DOMParser;
-} else {
-  const { JSDOM } = require('jsdom');
-  const { window } = new JSDOM('');
-  DOMParser = window.DOMParser;
-}
-
-const parser = new DOMParser();
+const parser = new window.DOMParser();
 
 const parseDom = markup => {
   const doc = parser.parseFromString(
