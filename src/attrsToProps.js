f5bd5ced27eff2f50da5a3dd88603c981178c648
Jacob Buck <buck.jacob@gmail.com>
2019-02-23 21:53:26 +1300

    Filtering

--- src/attrsToProps.js
+++ src/attrsToProps.js
@@ -2,25 +2,27 @@ import cssToStyle from 'css-to-style';
 import attrToPropName from './attrToPropName';
 
 const attrsToProps = attrs =>
-  Array.from(attrs).reduce((props, attr) => {
-    let { name, value } = attr;
+  Array.from(attrs)
+    .filter(
+      // Disallow script element nodes
+      attr => attr.name.substr(0, 2) !== 'on'
+    )
+    .reduce((props, attr) => {
+      let { name, value } = attr;
 
-    // Disallow event attributes
-    if (name.substr(0, 2) === 'on') return props;
+      let propName;
+      if (['aria-', 'data-'].includes(name.substr(0, 5))) {
+        propName = name;
+      } else if ('style' === name) {
+        propName = 'style';
+        value = cssToStyle(value);
+      } else {
+        propName = attrToPropName(name) || name;
+      }
 
-    let propName;
-    if (['aria-', 'data-'].includes(name.substr(0, 5))) {
-      propName = name;
-    } else if ('style' === name) {
-      propName = 'style';
-      value = cssToStyle(value);
-    } else {
-      propName = attrToPropName(name) || name;
-    }
+      props[propName] = value === '' ? true : value;
 
-    props[propName] = value === '' ? true : value;
-
-    return props;
-  }, {});
+      return props;
+    }, {});
 
 export default attrsToProps;
