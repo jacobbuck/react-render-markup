18348da3d184441dfc273f7e1ef30bdc4d30a3a3
Jacob Buck <buck.jacob@gmail.com>
2019-02-23 21:13:31 +1300

    Add type checking to renderMarkup

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import parseDom from './parse-dom';
 import domToVdom from './dom-to-vdom';
 
-const renderMarkup = (html, options = {}) =>
-  html && typeof html === 'string' ? domToVdom(parseDom(html), options) : null;
+const renderMarkup = (markup = '', options = {}) => {
+  if (typeof markup !== 'string') {
+    throw new TypeError('Expected parameter "markup" to be a string.');
+  }
+  return domToVdom(parseDom(markup), options);
+};
 
 export default renderMarkup;
