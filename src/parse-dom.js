ccb78c8a9b714301307a505122025b7be30fa3f2
Jacob Buck <buck.jacob@gmail.com>
2019-02-23 20:59:22 +1300

    Parse as html, not xml

--- src/parse-dom.js
+++ src/parse-dom.js
@@ -10,7 +10,7 @@ const parser = new DOMParser();
 const parseDom = markup => {
   const doc = parser.parseFromString(
     `<!DOCTYPE html>\n<html><body>${markup}</body></html>`,
-    'application/xhtml+xml'
+    'text/html'
   );
 
   return doc.getElementsByTagName('body')[0].childNodes;
