1856ef0ce6ee5da768c8f6171fc2a25d3a3e2f10
Jacob Buck <buck.jacob@gmail.com>
2020-06-06 17:23:30 +1200

    Replace parseDom.js with dom-parse package
    Also remove any browser/server specific code as that's now handled inside dom-parse

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,11 @@
-import replace from '@rollup/plugin-replace';
 import babel from 'rollup-plugin-babel';
 
-export default [
-  {
-    input: 'src/index.js',
-    output: { file: 'lib/browser.js', format: 'cjs' },
-    external: ['css-to-style', 'html-void-elements', 'react'],
-    plugins: [replace({ 'process.env.BROWSER': true }), babel()],
+export default {
+  input: 'src/index.js',
+  output: {
+    file: 'lib/index.js',
+    format: 'cjs',
   },
-  {
-    input: 'src/index.js',
-    output: { file: 'lib/server.js', format: 'cjs' },
-    external: ['css-to-style', 'html-void-elements', 'jsdom', 'react'],
-    plugins: [replace({ 'process.env.BROWSER': false })],
-  },
-];
+  external: ['css-to-style', 'dom-parse', 'html-void-elements', 'react'],
+  plugins: [babel()],
+};
