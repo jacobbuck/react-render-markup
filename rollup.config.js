cb164fb92b3eadc0986190e6043c1d4a368721a2
Jacob Buck <buck.jacob@gmail.com>
2020-06-06 17:25:28 +1200

    Update rollup.config.js
    Replace cjs with es module

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,7 @@ export default {
   input: 'src/index.js',
   output: {
     file: 'lib/index.js',
-    format: 'cjs',
+    format: 'es',
   },
   external: ['css-to-style', 'dom-parse', 'html-void-elements', 'react'],
   plugins: [babel()],
