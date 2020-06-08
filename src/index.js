efbaf892ee2ec78cf6070219fcf262a7b78e6f32
Jacob Buck <buck.jacob@gmail.com>
2020-06-08 19:17:31 +1200

    Don't type check in production :)

--- src/index.js
+++ src/index.js
@@ -6,11 +6,13 @@ const renderMarkup = (markup, options = {}) => {
   if (isNil(markup)) {
     return null;
   }
-  if (typeof markup !== 'string') {
-    throw new TypeError('Expected parameter "markup" to be a string.');
-  }
-  if (typeof options !== 'object') {
-    throw new TypeError('Expected parameter "options" to be an object.');
+  if (process.env.NODE_ENV !== 'production') {
+    if (typeof markup !== 'string') {
+      throw new TypeError('Expected parameter "markup" to be a string.');
+    }
+    if (typeof options !== 'object') {
+      throw new TypeError('Expected parameter "options" to be an object.');
+    }
   }
   return nodesToElements(parseDom(markup), options);
 };
