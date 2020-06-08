d859fe20c926c24b86ea023be59a808ad0f8f2fd
Jacob Buck <buck.jacob@gmail.com>
2020-06-08 19:04:05 +1200

    Refactor nodesToElements to use cond function

--- src/nodesToElements.js
+++ src/nodesToElements.js
@@ -1,50 +1,62 @@
 import React from 'react';
 import { ELEMENT_NODE, TEXT_NODE } from './constants/nodeTypes';
+import cond from './utilities/cond';
 import has from './utilities/has';
 import isNil from './utilities/isNil';
 import attrsToProps from './attrsToProps';
+import nodesToElements from './nodesToElements';
 
-const nodesToElements = (nodeList, options) => {
-  const { replace } = options;
+const nodeToElement = cond([
+  // Render text nodes as string
+  [(node) => node.nodeType === TEXT_NODE, (node) => node.textContent],
 
-  return Array.from(nodeList)
-    .filter(
-      // Only allow element and text nodes
-      (node) => node.nodeType === ELEMENT_NODE || node.nodeType === TEXT_NODE
-    )
-    .filter(
-      // Disallow script element nodes
-      (node) => node.nodeName.toLowerCase() !== 'script'
-    )
-    .map((node, i) => {
-      // Render text nodes as string
-      if (node.nodeType === TEXT_NODE) return node.textContent;
+  // Only allow element node types
+  [(node) => node.nodeType !== ELEMENT_NODE, () => null],
 
-      const tagName = node.nodeName.toLowerCase();
+  // Disallow script element nodes
+  [(node) => node.nodeName.toLowerCase() !== 'script', () => null],
+
+  // Handle replace option
+  [
+    (node, i, options) =>
+      !isNil(options.replace) &&
+      has(node.nodeName.toLowerCase(), options.replace),
+    (node, i, options) => {
+      const replaceNodeType = options.replace[node.nodeName.toLowerCase()];
+
+      // Don't render falsey replacements
+      if (!replaceNodeType) return null;
 
       const props = attrsToProps(node.attributes);
       props.key = i;
 
-      if (isNil(replace) && has(tagName, replace)) {
-        const replaceNodeType = replace[tagName];
-
-        // Don't render falsey replacements
-        if (!replaceNodeType) return null;
-
-        return React.createElement(
-          replaceNodeType,
-          props,
-          nodesToElements(node.childNodes, options)
-        );
-      }
-
       return React.createElement(
-        tagName,
+        replaceNodeType,
         props,
         nodesToElements(node.childNodes, options)
       );
-    })
+    },
+  ],
+
+  // Render all other elements
+  [
+    () => true,
+    (node, i, options) => {
+      const props = attrsToProps(node.attributes);
+      props.key = i;
+
+      return React.createElement(
+        node.nodeName.toLowerCase(),
+        props,
+        nodesToElements(node.childNodes, options)
+      );
+    },
+  ],
+]);
+
+const nodesToElements = (nodeList, options) =>
+  Array.from(nodeList)
+    .map((node, i) => nodeToElement(node, i, options))
     .filter((node) => !isNil(node));
-};
 
 export default nodesToElements;
