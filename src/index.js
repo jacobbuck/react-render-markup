dee2e52a720e80e2a8b32ebea4dc357106fe0670
Jacob Buck <buck.jacob@gmail.com>
2020-06-09 18:22:09 +1200

    Make typechecking happen earlier

--- src/index.js
+++ src/index.js
@@ -3,18 +3,15 @@ import isNil from './utilities/isNil';
 import nodesToElements from './nodesToElements';
 
 const renderMarkup = (markup, options = {}) => {
-  if (isNil(markup)) {
-    return null;
-  }
   if (process.env.NODE_ENV !== 'production') {
-    if (typeof markup !== 'string') {
+    if (typeof markup !== 'string' && !isNil(markup)) {
       throw new TypeError('Expected parameter "markup" to be a string.');
     }
     if (typeof options !== 'object') {
       throw new TypeError('Expected parameter "options" to be an object.');
     }
   }
-  return nodesToElements(parseDom(markup), options);
+  return isNil(markup) ? null : nodesToElements(parseDom(markup), options);
 };
 
 export default renderMarkup;
