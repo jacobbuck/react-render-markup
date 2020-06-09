4aef801c504ae84f9c92ace775820aac51771c53
Jacob Buck <buck.jacob@gmail.com>
2020-06-09 18:29:01 +1200

    Update index.test.js
    :100: test coverage of `renderMarkup()`!

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -12,3 +12,34 @@ test('renders SVG from a string', () => {
   const testRenderer = TestRenderer.create(renderMarkup(svgScimitar));
   expect(testRenderer.toJSON()).toMatchSnapshot();
 });
+
+test('returns null when markup is null or undefined', () => {
+  expect(renderMarkup(null)).toBe(null);
+  expect(renderMarkup()).toBe(null);
+});
+
+test("throws an error when markup isn't a string", () => {
+  expect(() => renderMarkup(false)).toThrow(
+    TypeError('Expected parameter "markup" to be a string.')
+  );
+});
+
+test("throws an error when options isn't an object", () => {
+  expect(() => renderMarkup(null, () => {})).toThrow(
+    new TypeError('Expected parameter "options" to be an object.')
+  );
+});
+
+test("doesn't throw an error in production mode", () => {
+  const previousEnv = process.env;
+  process.env = { ...previousEnv, NODE_ENV: 'production' };
+
+  expect(() => renderMarkup(false)).not.toThrow(
+    TypeError('Expected parameter "markup" to be a string.')
+  );
+  expect(() => renderMarkup(null, () => {})).not.toThrow(
+    new TypeError('Expected parameter "options" to be an object.')
+  );
+
+  process.env = previousEnv;
+});
