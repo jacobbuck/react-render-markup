92345a7509d303f27404688d09c81d28991c7c5e
Jacob Buck <buck.jacob@gmail.com>
2020-06-09 20:32:06 +1200

    Bring back the cjs build! Add "module" to package.json.

--- rollup.config.js
+++ rollup.config.js
@@ -2,10 +2,10 @@ import babel from 'rollup-plugin-babel';
 
 export default {
   input: 'src/index.js',
-  output: {
-    file: 'lib/index.js',
-    format: 'es',
-  },
+  output: [
+    { file: 'lib/index.cjs.js', format: 'cjs' },
+    { file: 'lib/index.esm.js', format: 'esm' },
+  ],
   external: ['css-to-style', 'dom-parse', 'react'],
   plugins: [babel()],
 };
