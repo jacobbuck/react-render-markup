bc9476bea37466c8f51a781c1cbb01d0eeb88e9d
Jacob Buck <buck.jacob@gmail.com>
2020-06-09 18:13:03 +1200

    Add svg test

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -1,8 +1,14 @@
 import TestRenderer from 'react-test-renderer';
 import htmlKitchenSink from './__fixtures__/htmlKitchenSink';
+import svgScimitar from './__fixtures__/svgScimitar';
 import renderMarkup from '..';
 
 test('renders HTML from a string', () => {
   const testRenderer = TestRenderer.create(renderMarkup(htmlKitchenSink));
+  expect(testRenderer.toJSON()).toMatchSnapshot();
+});
+
+test('renders SVG from a string', () => {
+  const testRenderer = TestRenderer.create(renderMarkup(svgScimitar));
   expect(testRenderer.toJSON()).toMatchSnapshot();
 });
