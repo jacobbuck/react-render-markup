03785cecb17275fc2838d5d9d0ea725e12e874b5
Jacob Buck <buck.jacob@gmail.com>
2020-06-13 16:23:07 +1200

    Refactor and clean up attrsToProps. Add startsWith utility.

--- src/utilities.js
+++ src/utilities.js
@@ -19,5 +19,8 @@ export const has = (obj, name) =>
 // Checks if the input value is null or undefined.
 export const isNil = (value) => value == null;
 
+// Checks if a string starts with the characters of a specified string.
+export const startsWith = (string, search) => string.indexOf(search) === 0;
+
 // Converts an array-like value into an array
 export const toArray = (value) => Array.prototype.slice.call(value);
