d618cfeeb37a45c1641bdf2de5784f827866881e
Jacob Buck <buck.jacob@gmail.com>
2020-06-13 16:25:58 +1200

    Refactor and clean up nodesToElements.
    This no longer makes nodesToElements functional, but instead it reduces 
    complexity and package size.
    
    It also includes an includes utility (hehe) as IE11 does't support 
    Array.prototype.includes

--- src/utilities.js
+++ src/utilities.js
@@ -1,20 +1,9 @@
-// Returns a function that always returns the given value.
-export const always = (value) => () => value;
-
-export const cond = (pairs) => (...args) => {
-  let i = 0;
-  while (i < pairs.length) {
-    const [predicate, transformer] = pairs[i];
-    if (predicate(...args)) {
-      return transformer(...args);
-    }
-    i += 1;
-  }
-};
-
 // Checks if an object has an own property with the specified name
 export const has = (obj, name) =>
   Object.prototype.hasOwnProperty.call(obj, name);
+
+// Checks if an array contains a value
+export const includes = (arr, search) => arr.indexOf(search) !== -1;
 
 // Checks if the input value is null or undefined.
 export const isNil = (value) => value == null;
