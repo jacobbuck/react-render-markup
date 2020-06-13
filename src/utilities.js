052212ffb08e2622781fd3c88603f7dddac6cd28
Jacob Buck <buck.jacob@gmail.com>
2020-06-13 16:44:28 +1200

    Call prototype methods in utilies directly

--- src/utilities.js
+++ src/utilities.js
@@ -3,13 +3,15 @@ export const has = (obj, name) =>
   Object.prototype.hasOwnProperty.call(obj, name);
 
 // Checks if an array contains a value.
-export const includes = (arr, search) => arr.indexOf(search) !== -1;
+export const includes = (arr, search) =>
+  Array.prototype.indexOf.call(arr, search) !== -1;
 
 // Checks if the input value is null or undefined.
 export const isNil = (value) => value == null;
 
 // Checks if a string starts with the characters of a specified string.
-export const startsWith = (string, search) => string.indexOf(search) === 0;
+export const startsWith = (string, search) =>
+  String.prototype.indexOf.call(string, search) === 0;
 
 // Converts an array-like value into an array.
 export const toArray = (value) => Array.prototype.slice.call(value);
