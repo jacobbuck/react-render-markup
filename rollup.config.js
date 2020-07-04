4819ced2c67ee7191266bbaf6a31d91d5b929906
Jacob Buck <buck.jacob@gmail.com>
2020-07-04 12:26:33 +1200

    Update rollup.config.js
    Add 'react-display-name' to external

--- rollup.config.js
+++ rollup.config.js
@@ -6,6 +6,6 @@ export default {
     { file: 'lib/index.cjs.js', format: 'cjs' },
     { file: 'lib/index.esm.js', format: 'esm' },
   ],
-  external: ['css-to-style', 'dom-parse', 'react'],
+  external: ['css-to-style', 'dom-parse', 'react', 'react-display-name'],
   plugins: [babel()],
 };
