c773757aa5100d1983c0b53e15a8b9c591e7019e
Jacob Buck <buck.jacob@gmail.com>
2020-08-18 20:25:50 +1200

    Update renderMarkup.js
    Return null for all falsey values

--- src/renderMarkup.js
+++ src/renderMarkup.js
@@ -11,8 +11,7 @@ const renderMarkup = (markup, options = {}) => {
       throw new TypeError('Expected parameter "options" to be an object.');
     }
   }
-
-  return isNil(markup) ? null : nodesToElements(parseDom(markup), options);
+  return markup ? nodesToElements(parseDom(markup), options) : null;
 };
 
 export default renderMarkup;
