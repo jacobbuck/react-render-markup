9c547606321e087340a92504fe285124bede43ca
Jacob <jacobbuck@users.noreply.github.com>
2020-09-25 23:17:26 +1200

    Update import of react to use named exports (rather than default export)

--- src/Markup.js
+++ src/Markup.js
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import React from 'react';
+import * as React from 'react';
 import renderMarkup from './renderMarkup';
 
 const Markup = React.memo(function Markup(props) {
