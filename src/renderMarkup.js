31c1f2e1a31619b7378467c89b29c7e509b03946
Jacob Buck <buck.jacob@gmail.com>
2020-10-18 09:12:15 +0000

    Improve TypeError messages

--- src/renderMarkup.js
+++ src/renderMarkup.js
@@ -6,12 +6,12 @@ const renderMarkup = (markup, options = {}) => {
   if (process.env.NODE_ENV !== 'production') {
     if (typeof markup !== 'string' && !isNil(markup)) {
       throw new TypeError(
-        `Expected "markup" to be a string, not ${typeof markup}.`
+        `Expected \`markup\` to be of type \`string\`, but received type \`${typeof markup}\``
       );
     }
     if (typeof options !== 'object') {
       throw new TypeError(
-        `Expected "options" to be an object, not ${typeof options}.`
+        `Expected \`options\` to be of type \`object\`, but received type \`${typeof options}\``
       );
     }
   }
