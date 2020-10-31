319a35c16fe558148329b0bd176f2eaf3d06f747
Jacob Buck <buck.jacob@gmail.com>
2020-10-31 23:48:57 +0000

    Remove console.log used for debugging 😅

--- src/__tests__/attrsToProps.test.js
+++ src/__tests__/attrsToProps.test.js
@@ -1,10 +1,6 @@
 import attrsToProps from '../attrsToProps';
 
 test('converts attributes to props', () => {
-  console.log(
-    Object.prototype.toString.call(document.createElement('p').ownerDocument)
-  );
-
   const attrs = [
     { name: 'class', value: 'foo bar' },
     { name: 'id', value: 'baz' },
