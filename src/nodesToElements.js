96bf1b293470a2628a42d617c484d5335ef6789f
Jacob Buck <buck.jacob@gmail.com>
2020-11-01 09:32:02 +0000

    Refactor nodesToElements.js to `continue` less and reduce lines

--- src/nodesToElements.js
+++ src/nodesToElements.js
@@ -6,57 +6,46 @@ import nodeNameToType from './nodeNameToType';
 
 const nodesToElements = (nodeList, options) => {
   const tree = [];
-
   for (let i = 0; i < nodeList.length; i++) {
     const node = nodeList[i];
-
-    // Handle text nodes.
-    if (node.nodeType === TEXT_NODE) {
+    // Only render element nodes and text nodes.
+    if (node.nodeType === ELEMENT_NODE) {
+      let type = nodeNameToType(node.nodeName);
+      if (
+        // Never render <script> elements.
+        type === 'script' ||
+        // Handle allowed option to only render elements that are allowed.
+        (options.allowed && options.allowed.includes(type))
+      ) {
+        continue;
+      }
+      // Handle replace option.
+      if (
+        options.replace &&
+        Object.prototype.hasOwnProperty.call(options.replace, type)
+      ) {
+        type = options.replace[type];
+        // Don't render falsey replacements.
+        if (!type) {
+          continue;
+        }
+      }
+      const props = attrsToProps(node.attributes);
+      props.key = `${getDisplayName(type)}-${i}`;
+      tree.push(
+        React.createElement(
+          type,
+          props,
+          nodesToElements(node.childNodes, options)
+        )
+      );
+    } else if (node.nodeType === TEXT_NODE) {
       // Handle trim option to remove whitespace text nodes.
       if (!options.trim || node.textContent.trim() !== '') {
         tree.push(node.textContent);
       }
-      continue;
     }
-
-    let type = nodeNameToType(node.nodeName);
-
-    // Only render element (and text) nodes.
-    if (
-      node.nodeType !== ELEMENT_NODE ||
-      // Never render <script> elements.
-      type === 'script' ||
-      // Handle allowed option to only render elements that are allowed.
-      (options.allowed && !options.allowed.includes(type))
-    ) {
-      continue;
-    }
-
-    // Handle replace option.
-    if (
-      options.replace &&
-      Object.prototype.hasOwnProperty.call(options.replace, type)
-    ) {
-      type = options.replace[type];
-
-      // Don't render falsey replacements.
-      if (!type) {
-        continue;
-      }
-    }
-
-    const props = attrsToProps(node.attributes);
-    props.key = `${getDisplayName(type)}-${i}`;
-
-    tree.push(
-      React.createElement(
-        type,
-        props,
-        nodesToElements(node.childNodes, options)
-      )
-    );
   }
-
   return tree.length > 0 ? tree : null;
 };
 
