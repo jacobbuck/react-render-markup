464a5ba749e712cb6077853cbf7f0560aa41aa82
Jacob Buck <buck.jacob@gmail.com>
2023-05-09 04:35:32 +0000

    Replace build with vite

---
+++ vite.config.js
@@ -0,0 +1,24 @@
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.js'),
+      formats: ['cjs', 'es'],
+    },
+    rollupOptions: {
+      external: [
+        'css-to-style',
+        'dom-parse',
+        'prop-types',
+        'react',
+        'react-display-name',
+        'tiny-invariant',
+      ],
+    },
+    sourcemap: true,
+  },
+  plugins: [react()],
+});
