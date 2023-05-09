084feb34df58dfefb445b66eb01ccebd27d3d084
Jacob Buck <buck.jacob@gmail.com>
2023-05-09 10:06:58 +0000

    Add esbuild-jest. Remove babel.

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,8 @@
-module.exports = {
+export default {
   coveragePathIgnorePatterns: ['/node_modules/', '/__fixtures__/'],
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['/node_modules/', '/__fixtures__/'],
+  transform: {
+    '^.+\\.jsx?$': 'esbuild-jest',
+  },
 };
