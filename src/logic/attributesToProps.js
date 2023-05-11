412098dd0db97d0c255695358f8ccc30345ba66a
Jacob Buck <buck.jacob@gmail.com>
2023-05-11 05:40:47 +0000

    Refactor attributesToProps internals. Only cast standard boolean props.

--- src/logic/attributesToProps.js
+++ src/logic/attributesToProps.js
@@ -1,26 +1,29 @@
 import cssToStyle from 'css-to-style';
+import { booleanProps } from '../constants/booleanProps';
 import { standardProps } from '../constants/standardProps';
 import { reactProps } from '../constants/reactProps';
 
 export const attributesToProps = (attrs) => {
   const props = {};
   for (let i = 0; i < attrs.length; i++) {
-    const { name, value } = attrs[i];
+    let { name, value } = attrs[i];
     // Disallow event attributes and react props.
     if (name.startsWith('on') || reactProps.includes(name)) {
       continue;
     }
-    // Don't modify aria-* or data-* attributes.
-    if (name.startsWith('aria-') || name.startsWith('data-')) {
-      props[name] = value;
-      continue;
+    // Get property name of HTML or SVG standard attributes
+    if (Object.hasOwn(standardProps, name)) {
+      name = standardProps[name];
     }
-    // Handle style attribute.
+    // Handle `style` attribute
     if (name === 'style') {
-      props[name] = cssToStyle(value);
-      continue;
+      value = cssToStyle(value);
     }
-    props[standardProps.get(name) ?? name] = value === '' ? true : value;
+    // Handle boolean attributes
+    if (booleanProps.includes(name)) {
+      value = true;
+    }
+    props[name] = value;
   }
   return props;
 };
