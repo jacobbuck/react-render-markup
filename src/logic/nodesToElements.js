8c2ef5fbb01f175fa1cbd0e39d5a250ffd1922bb
Jacob Buck <buck.jacob@gmail.com>
2023-05-11 05:47:44 +0000

    Update nodesToElements: - Recurse internal function - Destructure and scope options (instead of drilling)

--- src/logic/nodesToElements.js
+++ src/logic/nodesToElements.js
@@ -4,55 +4,58 @@ import { getDisplayName } from '../utils/getDisplayName';
 import { nodeNameToType } from '../utils/nodeNameToType';
 import { attributesToProps } from './attributesToProps';
 
-export const nodesToElements = (nodeList, options) => {
-  const tree = [];
-  for (let i = 0; i < nodeList.length; i++) {
-    const node = nodeList[i];
-    // Only render element nodes and text nodes.
-    if (node.nodeType === elementNode) {
-      let type = nodeNameToType(node.nodeName);
-      if (
-        // Never render <script> elements.
-        type === 'script' ||
-        // Handle allowed option to only render elements that are allowed.
-        (options.allowed &&
-          (typeof options.allowed === 'function'
-            ? !options.allowed(node)
-            : !options.allowed.includes(type)))
-      ) {
-        continue;
-      }
-      // Handle replace option.
-      if (options.replace) {
-        const replacement =
-          typeof options.replace === 'function'
-            ? options.replace(node)
-            : Object.hasOwn(options.replace, type)
-            ? options.replace[type]
-            : undefined;
-        // Don't render element if replacement is false or null.
-        if (replacement === false || replacement === null) {
+export const nodesToElements = (nodes, { allowed, replace, trim }) => {
+  const walkNodes = (nodes) => {
+    const tree = [];
+    for (let i = 0; i < nodes.length; i++) {
+      const node = nodes[i];
+      // Only render element nodes and text nodes.
+      if (node.nodeType === elementNode) {
+        let type = nodeNameToType(node.nodeName);
+        if (
+          // Never render <script> elements.
+          type === 'script' ||
+          // Handle allowed option to only render elements that are allowed.
+          (allowed &&
+            (typeof allowed === 'function'
+              ? !allowed(node)
+              : !allowed.includes(type)))
+        ) {
           continue;
         }
-        // Replace element replacement—if not undefined.
-        if (replacement !== undefined) {
-          type = replacement;
+        // Handle replace option.
+        if (replace) {
+          const replacement =
+            typeof replace === 'function'
+              ? replace(node)
+              : Object.hasOwn(replace, type)
+              ? replace[type]
+              : undefined;
+          // Don't render element if replacement is false or null.
+          if (replacement === false || replacement === null) {
+            continue;
+          }
+          // Replace element replacement—if not undefined.
+          if (replacement !== undefined) {
+            type = replacement;
+          }
+        }
+        const props = attributesToProps(node.attributes);
+        props.key = `${getDisplayName(type)}-${i}`;
+        const children = nodesToElements(node.childNodes);
+        tree.push(
+          isValidElement(type)
+            ? cloneElement(type, props, children)
+            : createElement(type, props, children)
+        );
+      } else if (node.nodeType === textNode) {
+        // Handle trim option to remove whitespace text nodes.
+        if (!trim || node.textContent.trim() !== '') {
+          tree.push(node.textContent);
         }
       }
-      const props = attributesToProps(node.attributes);
-      props.key = `${getDisplayName(type)}-${i}`;
-      const children = nodesToElements(node.childNodes, options);
-      tree.push(
-        isValidElement(type)
-          ? cloneElement(type, props, children)
-          : createElement(type, props, children)
-      );
-    } else if (node.nodeType === textNode) {
-      // Handle trim option to remove whitespace text nodes.
-      if (!options.trim || node.textContent.trim() !== '') {
-        tree.push(node.textContent);
-      }
     }
-  }
-  return tree.length > 0 ? tree : null;
+    return tree.length > 0 ? tree : null;
+  };
+  return walkNodes(nodes);
 };
