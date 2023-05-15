8106efff12af6b465d8b3923b979a89c19a8031d
Jacob Buck <buck.jacob@gmail.com>
2023-05-15 01:43:06 +0000

    Update getDisplayName to return display name of React Element type

--- src/utils/getDisplayName.js
+++ src/utils/getDisplayName.js
@@ -1,4 +1,13 @@
+import { isValidElement } from 'react';
+
+/**
+ * Returns the display name of a react component, element or type
+ * @param {*} component - any component or element value
+ * @returns {string} - display name or "Unknown"
+ */
 export const getDisplayName = (component) =>
-  typeof component === 'string'
-    ? component
-    : component.displayName || component.name || 'Unknown';
+  (typeof component === 'string' && component) ||
+  component.displayName ||
+  component.name ||
+  (isValidElement(component) && getDisplayName(component.type)) ||
+  'Unknown';
